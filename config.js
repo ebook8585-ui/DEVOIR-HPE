29d85acfaf61de0e01b449e0b9aee458d5636c62
ebook8585-ui <ebook8585@gmail.com>
2026-06-04 20:46:13 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// CONFIGURATION DE L'ENVOI AUTOMATIQUE
+// 1) Déployez le fichier CODE_GOOGLE_APPS_SCRIPT.gs dans Google Apps Script.
+// 2) Copiez l'URL de l'application Web.
+// 3) Collez l'URL entre les guillemets ci-dessous.
+
+window.QUIZ_CONFIG = {
+  OWNER_EMAIL: "ebook8585@gmail.com",
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwYl1MtrQC2nMnmbunXK5h8CZ4dUMcP6oUooOo0aR8HLGER7JKComDcGET4njTcQnfj8A/exec"
+};
